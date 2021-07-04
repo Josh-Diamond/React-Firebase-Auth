841c2dde5c7f7ebf2a1ab41a46ab2b9e2780e2dc
Josh Diamond <diamondroad91@gmail.com>
2021-07-03 19:56:56 -0700

    upload-success

--- react-firebase-auth/src/components/google-drive/Dashboard.js
+++ react-firebase-auth/src/components/google-drive/Dashboard.js
@@ -7,26 +7,37 @@ import Folder from './Folder'
 import { useParams, useLocation } from 'react-router-dom'
 import FolderBreadcrumbs from "./FolderBreadcrumbs";
 import AddFileBtn from "./AddFileBtn";
+import File from './File'
 
 export default function Dashboard() {
   const { folderId } = useParams()
   const {state = {} } = useLocation()
-  const { folder, childFolders } = useFolder(folderId, state.folder)
+  const { folder, childFolders, childFiles } = useFolder(folderId, state.folder)
 
   return (
     <>
       <Navigation />
       <Container fluid>
         <div className="d-flex align-items-center">
-        <FolderBreadcrumbs currentFolder={folder} />
-        <AddFileBtn currentFolder={folder} />
-        <AddFolderBtn currentFolder={folder} />
+          <FolderBreadcrumbs currentFolder={folder} />
+          <AddFileBtn currentFolder={folder} />
+          <AddFolderBtn currentFolder={folder} />
         </div>
+        {childFolders.length > 0 && childFiles.length > 0 && <hr />}
         {childFolders.length > 0 && (
           <div className='d-flex flex-wrap'>
             {childFolders.map(childFolder => (
               <div key={childFolder.id} style={{ maxWidth: '250px'}} className="p-2">
                 <Folder folder={childFolder} />
+              </div>
+            ))}
+          </div>
+        )}
+        {childFiles.length > 0 && (
+          <div className='d-flex flex-wrap'>
+            {childFiles.map(childFile => (
+              <div key={childFile.id} style={{ maxWidth: '250px'}} className="p-2">
+                <File file={childFile} />
               </div>
             ))}
           </div>
